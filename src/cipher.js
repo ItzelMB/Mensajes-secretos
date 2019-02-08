1b54d5d0ca5c8a493b91b112afa10242bb1dcd59
ItzelMB <it.mobec@gmail.com>
2019-02-08 12:40:47 -0600

    V1.0 concluída

--- src/cipher.js
+++ src/cipher.js
@@ -1,26 +1,23 @@
-const caesEncode = (offset, message) =>{
-    let result ='';
-    for(let i=0; i<message.length; i++){
-        let findAscii = message.charCodeAt(i);
-        let getPosition = (findAscii - 65 + offset) % 26 + 65;
-        let bringChar = String.fromCharCode(getPosition);
-        result += bringChar;
-    } 
-    return result;
-};
-
-const caesDecode = (offset, message) =>{
-    let result ='';
-    for(let i=0; i<message.length; i++){
-        let findAscii = message.charCodeAt(i);
-        let getPosition = (findAscii + 65 - offset) % 26 + 65;
-        let bringChar = String.fromCharCode(getPosition);
-        result += bringChar;
-    } 
-    return result;
-};
-
 window.cipher = {
-    encode: caesEncode,
-    decode: caesDecode,
+    encode: function(offset, message) {
+        let result ='';
+        for(let i=0; i<message.length; i++){
+            let findAscii = message.charCodeAt(i);
+            let getPosition = (findAscii - 65 + offset) % 26 + 65;
+            let bringChar = String.fromCharCode(getPosition);
+            result += bringChar;
+        } 
+        return result;
+    },
+    
+    decode: function(offset, message){
+        let result ='';
+        for(let i=0; i<message.length; i++){
+            let findAscii = message.charCodeAt(i);
+            let getPosition = (findAscii + 65 - offset) % 26 + 65;
+            let bringChar = String.fromCharCode(getPosition);
+            result += bringChar;
+        } 
+        return result;
+    }
   };
